9e193faf4e10b16f6f84918bdb4e395b76a37a19
Fredrik Lillemoen Eiding <fueredoriku@gmail.com>
2024-11-11 00:28:06 +0100

    Add very simple mesh coloring
    - Todo: Ignore drawing green when depth difference is too great

---
+++ Assets/Tracking/MeshColorEmitter.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class MeshColorEmitter : MonoBehaviour
+{
+    [SerializeField]
+    private Transform spawnPoint;
+    [SerializeField]
+    private GameObject meshColoringPrefab;
+    [SerializeField]
+    private LayerMask colorLayer;
+    private GameObject objectPool;
+    private bool isSpawning = false;
+
+    public void SpawnColoring()
+    {
+        isSpawning = true;
+        objectPool = new("objectPool");
+        StartCoroutine(Spawner());
+    }
+
+    private IEnumerator Spawner()
+    {
+        while (isSpawning) {
+            if (Physics.OverlapSphere(spawnPoint.transform.position, 0.1f, colorLayer).Any())
+            {
+                var colorObject = Instantiate(meshColoringPrefab);
+                colorObject.transform.position = spawnPoint.position;
+                colorObject.transform.SetParent(objectPool.transform);
+            }
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+
+    public void StopColoring()
+    {
+        isSpawning = false;
+        Destroy(objectPool);
+    }
+}
