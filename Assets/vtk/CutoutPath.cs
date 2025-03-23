99bc4bd720a622a79fd8b649a214b20f74b10dba
Fredrik <fueredoriku@gmail.com>
2025-03-23 22:27:52 +0100

    Add unfinished normal to centerline cutoff plane

--- Assets/vtk/CutoutPath.cs
+++ Assets/vtk/CutoutPath.cs
@@ -9,16 +9,21 @@ public class CutoutPath : MonoBehaviour
     private PathToTumorVisualizer path;
     private int index = 0;
     private Vector3[] travelPath;
+    Vector3 oldPositon;
     private void Start()
     {
         travelPath = path.SampledPath.ToArray();
         transform.SetLocalPositionAndRotation(path.positionOffset, Quaternion.Euler(path.rotationOffset));
+        oldPositon = travelPath[index];
     }
     void Update()
     {
-        transform.GetChild(0).localPosition = -travelPath[index];
+        Vector3 currentPosition = -travelPath[index];
+        transform.GetChild(0).localPosition = currentPosition;
+        transform.GetChild(0).up = Vector3.Cross(transform.right, oldPositon - currentPosition);
         index++;
         if (index == travelPath.Length-1)
             index = 0;
+        oldPositon = currentPosition;
     }
 }
