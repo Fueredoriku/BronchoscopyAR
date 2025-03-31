637bd5f397971bb2026fac14f2cd58cdccbb6720
Fredrik Lillemoen Eiding <fueredoriku@gmail.com>
2025-03-31 21:11:34 +0200

    Add M2Mqtt unity client with normalized path value

---
+++ Assets/Networking/M2MClient.cs
@@ -0,0 +1,108 @@
+using M2MqttUnity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+public class M2MClient : M2MqttUnityClient
+{
+    [Tooltip("Set this to true to perform a testing cycle automatically on startup")]
+    public bool autoTest = false;
+    [SerializeField]
+    private bool updatePath;
+    private List<string> eventMessages = new List<string>();
+    private Vector3 bronchoscopePosition = Vector3.zero;
+    public Vector3 BronchoscopePosition => bronchoscopePosition;
+    [SerializeField]
+    private CutoutPath cutoutPath;
+    // TODO: Recieve and handle bronchoscope position
+    // TODO: Recieve each piece of selected anatomy
+
+    protected override void DecodeMessage(string topic, byte[] message)
+    {
+        string msg = System.Text.Encoding.UTF8.GetString(message);
+        Debug.Log("Received: " + msg);
+        switch (topic)
+        {
+            case "M2MQTT_Unity/bronchoscope":
+                try
+                {
+                    // TODO: This format instead
+                    /*
+                    string[] numbers = msg.Split(",");
+                    float x = float.Parse(numbers[0]);
+                    float y = float.Parse(numbers[1]);
+                    float z = float.Parse(numbers[2]);
+                    bronchoscopePosition = new Vector3(x, y, z);
+                    */
+                    cutoutPath.NormalizedPathPosition = float.Parse(msg);
+                }
+                catch 
+                {
+                    Debug.Log("Bronchoscope position was not formated correctly, expected Vector3");
+                }
+                break;
+            case "M2MQTT_Unity/anatomy":
+                // TODO implement obj parser
+                break;
+            default:
+                break;
+        }
+    }
+    protected override void OnConnecting()
+    {
+        base.OnConnecting();
+        Debug.Log("Connecting to broker on " + brokerAddress + ":" + brokerPort.ToString() + "...\n");
+    }
+
+    protected override void OnConnected()
+    {
+        base.OnConnected();
+        Debug.Log("Connected to broker on " + brokerAddress + "\n");
+        //TODO: Subscribe path?
+    }
+    protected override void OnConnectionFailed(string errorMessage)
+    {
+        Debug.Log("CONNECTION FAILED! " + errorMessage);
+    }
+
+    protected override void Update()
+    {
+        base.Update(); // call ProcessMqttEvents()
+
+        if (eventMessages.Count > 0)
+        {
+            foreach (string msg in eventMessages)
+            {
+                ProcessMessage(msg);
+            }
+            eventMessages.Clear();
+        }
+        if (updatePath)
+        {
+            //UpdatePath();
+        }
+    }
+
+    private void ProcessMessage(string msg)
+    {
+        //AddUiMessage("Received: " + msg);
+    }
+
+    private void OnDestroy()
+    {
+        Disconnect();
+    }
+
+    protected override void SubscribeTopics()
+    {
+        client.Subscribe(new string[] { "M2MQTT_Unity/bronchoscope" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+        client.Subscribe(new string[] { "M2MQTT_Unity/anatomy" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+    }
+
+    protected override void UnsubscribeTopics()
+    {
+        client.Unsubscribe(new string[] { "M2MQTT_Unity/bronchoscope" });
+        client.Unsubscribe(new string[] { "M2MQTT_Unity/anatomy" });
+    }
+}
