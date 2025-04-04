85a6c7a4266c5657aaac3b680d594eecfdaf3029
Fredrik Lillemoen Eiding <fueredoriku@gmail.com>
2025-04-04 16:09:47 +0200

    Add origin offset for path cutout

--- Assets/vtk/CutoutPath.cs
+++ Assets/vtk/CutoutPath.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CutoutPath : MonoBehaviour
 {
@@ -22,15 +23,25 @@ public class CutoutPath : MonoBehaviour
     public float NormalizedPathPosition = 0f;
     public CutOutDirection CutOutMode = CutOutDirection.objectUp;
 
+    [SerializeField]
+    private Transform anatomyParent;
+    [SerializeField]
+    private Transform anatomyHolder;
+    [SerializeField]
+    private Transform cutoutHolder;
+    [SerializeField]
+    private Transform cutoutTransform;
     private Quaternion relativeToParentRotation;
     private Quaternion relativeDirection;
+    [SerializeField]
+    private float testScale = 0.1f;
     private void Start()
     {
         travelPath = path.SampledPath.ToArray();
         transform.SetLocalPositionAndRotation(path.positionOffset, Quaternion.Euler(path.rotationOffset));
         oldPosition = travelPath[index];
-        relativeToParentRotation = transform.GetChild(0).localRotation;
-        relativeDirection = transform.GetChild(0).GetChild(0).localRotation;
+        relativeToParentRotation = cutoutHolder.localRotation;
+        relativeDirection = cutoutTransform.localRotation;
     }
     void Update()
     {
@@ -38,35 +49,47 @@ public class CutoutPath : MonoBehaviour
         Vector3 currentPosition = -travelPath[index];
         oldPosition = -travelPath[index - 1];
         Vector3 nextPosition = -travelPath[index + 1];
-        transform.GetChild(0).localPosition = currentPosition;
-
+        //anatomyHolder.localPosition = cutoutHolder.rotation * -(currentPosition * testScale);
         switch (CutOutMode)
         {
             case CutOutDirection.objectUp:
-                transform.GetChild(0).rotation = transform.parent.parent.rotation;
-                transform.GetChild(0).GetChild(0).localRotation = relativeDirection;
+                cutoutHolder.localPosition = currentPosition;
+                cutoutHolder.rotation = anatomyParent.rotation;
+                cutoutTransform.localRotation = relativeDirection;
                 break;
             case CutOutDirection.objectForward:
-                transform.GetChild(0).localRotation = relativeToParentRotation;
-                transform.GetChild(0).GetChild(0).localRotation = relativeDirection;
+                cutoutHolder.localPosition = currentPosition;
+                cutoutHolder.localRotation = relativeToParentRotation;
+                cutoutTransform.localRotation = relativeDirection;
                 break;
             case CutOutDirection.camera:
                 // TODO: All anatomy needs a holder parent which must have a postion offset akin to the pathpostion!!!
                 // This must be done before rotating so the origin is adjuster for where on the path we are.
-                transform.parent.parent.LookAt(Camera.main.transform);
                 //transform.parent.parent.localRotation *= Quaternion.FromToRotation(Vector3.up, Vector3.forward);
                 //transform.parent.parent.localRotation *= Quaternion.FromToRotation(oldPosition - currentPosition + (currentPosition - nextPosition), Vector3.up);
-                transform.GetChild(0).localRotation = relativeToParentRotation;
-                transform.GetChild(0).GetChild(0).localRotation = Quaternion.FromToRotation(Vector3.down, oldPosition - currentPosition + (currentPosition - nextPosition));
-                transform.parent.parent.forward = -transform.GetChild(0).GetChild(0).up;
+                //cutoutHolder.LookAt(Camera.main.transform);
+                //cutoutTransform.localRotation = Quaternion.FromToRotation(Vector3.up, Vector3.back);
+                anatomyHolder.localPosition = Quaternion.FromToRotation(Vector3.up, Vector3.back) * Quaternion.FromToRotation(Vector3.forward, Vector3.back) * (-currentPosition * 0.25f);
+                cutoutHolder.SetParent(anatomyParent);
+                cutoutTransform.localRotation = Quaternion.FromToRotation(Vector3.down, oldPosition - currentPosition + (currentPosition - nextPosition));
+                //cutoutHolder.localRotation = relativeToParentRotation;
+                //anatomyHolder.localRotation = Quaternion.FromToRotation(Vector3.down, oldPosition - currentPosition + (currentPosition - nextPosition));
+
+                //anatomyParent.LookAt(Camera.main.transform);
+                /*
+                anatomyParent.rotation = Quaternion.FromToRotation(Vector3.forward, anatomyParent.position - Camera.main.transform.position)
+                    * Quaternion.Inverse(Quaternion.FromToRotation(Vector3.down, oldPosition - currentPosition + (currentPosition - nextPosition)));
+                */
                 break;
             case CutOutDirection.pathDirection:
-                transform.GetChild(0).localRotation = relativeToParentRotation;
-                transform.GetChild(0).GetChild(0).localRotation = Quaternion.FromToRotation(Vector3.down, oldPosition - currentPosition + (currentPosition - nextPosition)); 
+                cutoutHolder.localPosition = currentPosition;
+                cutoutHolder.localRotation = relativeToParentRotation;
+                cutoutTransform.localRotation = Quaternion.FromToRotation(Vector3.down, oldPosition - currentPosition + (currentPosition - nextPosition)); 
                 break;
             case CutOutDirection.pathDirectionTilted:
-                transform.GetChild(0).localRotation = Quaternion.FromToRotation(Vector3.up, Vector3.forward);
-                transform.GetChild(0).GetChild(0).localRotation = Quaternion.FromToRotation(Vector3.down, oldPosition - currentPosition + (currentPosition - nextPosition));
+                cutoutHolder.localPosition = currentPosition;
+                cutoutHolder.localRotation = Quaternion.FromToRotation(Vector3.up, Vector3.forward);
+                cutoutTransform.localRotation = Quaternion.FromToRotation(Vector3.down, oldPosition - currentPosition + (currentPosition - nextPosition));
                 break;
             default:
                 break;
